e947d064b84afe958cf89ffc8f7910d2fb6330ab
Valentino Geronzi <valentinogeronzi@hotmail.com>
2022-08-16 09:30:05 -0300

    Js terminado

--- cart.js
+++ cart.js
@@ -1,23 +1,96 @@
+
+const shoppingCart = JSON.parse(localStorage.getItem('products')) || [];
+
 const cartContainer = document.querySelector(".cart_container");
-/* const cart = JSON.parse(localStorage.getItem('carrito')) || [];
- */
-const addCartBtn = document.getElementsByClassName("addCart");
-console.log(addCartBtn)
 
-for (let btn of addCartBtn) {
-    btn.addEventListener("click", collectBtnData);
+function mostrarEnCarrito() {
+    cartContainer.innerHTML = ""
+    shoppingCart.forEach(p => {
+
+        let div = document.createElement("div")
+        div.className = "productInCart"
+        div.innerHTML = `<div class="prod_in_cart" id="${p.id}">
+                                        <p class="epigrafe" id="name">${p.name}</p>
+                                        <img src="../../${p.src}" class="img-product-cart"/>
+                                        <div class="epigrafe-container">
+                                            <p class="epigrafe" id="price">$${p.price}</p>
+                                            <button class="remove_prod_cart">Eliminar</button>
+                                            <p class="cantidad">Cantidad: ${p.quantity}</p>
+                                        </div>
+                                   </div>`
+
+        cartContainer.appendChild(div)
+
+        let botonesEliminar = document.querySelectorAll(".remove_prod_cart")
+        for (let boton of botonesEliminar) {
+            boton.addEventListener("click", eliminarProducto)
+        }
+
+    })
 }
 
-function collectBtnData(e) {
+mostrarEnCarrito()
+
+
+
+function eliminarProducto(e) {
     let btn = e.target
-    let prod = btn.parentElement;
-    let name = prod.querySelector("#name").innerText;
-    let img = prod.querySelector(".img-product").src;
-    let price = prod.querySelector("#price").innerText;
-    console.log(name, img, price)
-    addToCart(name, img, price)
+    shoppingCart.splice(shoppingCart.findIndex(producto => producto.id == btn.parentElement.parentElement.id), 1);
+    localStorage.setItem('products', JSON.stringify(shoppingCart))
+    mostrarEnCarrito()
+    cartQuantity()
+    finalPrice()
 }
 
-function addToCart(name, img, price){
-    
+
+
+let quantitySpan = document.querySelector(".cart_quantity");
+let btnComprar = document.querySelector(".btn_comprar")
+
+function cartQuantity() {
+    if (shoppingCart.length === 0) {
+        cartContainer.innerHTML = `<div class="no-prod"> No hay productos en el carrito 🛒 </div>`
+
+        btnComprar.addEventListener("click", e => {
+            e.preventDefault()
+            Swal.fire({ icon: 'error', background: 'rgb(11, 4, 15)', confirmButtonColor: 'rgb(60, 38, 70)', color: '#eeeeed', title: 'No hay productos en el carrito!' })
+        });
+    }
+
+    let total = shoppingCart.reduce((acc, e) => acc + e.quantity, 0)
+    quantitySpan.innerHTML = `${total}`;
+    localStorage.setItem('products', JSON.stringify(shoppingCart))
 }
+cartQuantity()
+
+btnComprar.addEventListener("click", e => {
+    e.preventDefault()
+    if (shoppingCart.length > 0) {
+        Swal.fire({ icon: 'success', background: 'rgb(11, 4, 15)', confirmButtonColor: 'rgb(60, 38, 70)', color: '#eeeeed', title: 'Comprado!' })
+    } 
+    vaciarCarrito()
+});
+
+function vaciarCarrito() {
+    shoppingCart.splice(0, shoppingCart.length)
+    localStorage.setItem('products', JSON.stringify(shoppingCart))
+
+    mostrarEnCarrito()
+    cartQuantity()
+    finalPrice()
+}
+
+let finalPriceDiv = document.querySelector(".final_price");
+
+let totalCostArr = [];
+
+function finalPrice() {
+    let totalPrice = shoppingCart.reduce((acc, e) => acc + e.quantity * e.price, 0)
+    finalPriceDiv.innerText = `${totalPrice}`;
+    localStorage.setItem('products', JSON.stringify(shoppingCart))
+}
+finalPrice()
+
+
+
+
--- script.js
+++ script.js
@@ -1,128 +1,85 @@
-/* let salir = "N";
 
-const calcPrecio = () => {
-    while (salir == "N" || salir == "n") {
-
-        let opcion = prompt("Precio en Dolares o Pesos?")
-
-        if (opcion == "Pesos" || opcion == "pesos") {
-            let precioProd = prompt("Precio de producto en USD");
-
-            let dolar = prompt("Precio del dolar");
-
-            precioProd *= dolar;
-            precioProd *= 1.2;
-
-        }else if (opcion == "Dolares" || opcion == "dolares"){
-
-            let precioProd = prompt("Precio de producto");
-            
-            precioProd *= 1.2;
-
-        }
-        
-    }
-    alert(`Precio: $${precioProd}`)
-    salir = prompt("Desea salir? S/N");
-}
-
-calcPrecio();
- */
-
-/* const products = [
-    { name: "mouse", price: 15 },
-    { name: "keyboard", price: 35 },
-    { name: "monitor", price: 100 }
-]
-
-class Product {
-    constructor(name, price) {
-        this.name = name;
-        this.price = parseInt(price);
-    }
-
-}
-
-const newProduct = (n, p) => {
-    newProd = new Product(n, p);
-    products.push(newProd);
-}
-
-
-let salir = "N";
-
-const calcPrecio = () => {
-    while (salir == "N" || salir == "n") {
-
-        let opcion = prompt("Que desea hacer? 'VER': para ver precio de un producto / 'NUEVO': para agregar un nuevo producto")
-
-        if (opcion == "ver" || opcion == "Ver" || opcion == "VER") {
-            let prodName = prompt("Nombre del producto");
-
-            let nameSearch = products.find(p => p.name == prodName)
-            if (nameSearch) {
-                alert(`Precio de ${prodName}: $${nameSearch.price}`);
-
-            } else {
-                alert("Producto no encontrado");
-            }
-
-        } else if (opcion == "nuevo" || opcion == "Nuevo" || opcion == "NUEVO") {
-            let prodName = prompt("Nombre del producto");
-            let prodPrice = prompt("Precio del producto");
-
-            let dolar = 280;
-
-            prodPrice *= dolar;
-            prodPrice *= 1.2;
-
-            newProduct(prodName, prodPrice);
-
-            alert("Agregado!")
-        } else {
-            alert("Introduzca un valor valido")
-        }
-        salir = prompt("Desea salir? ✔️S/N❌");
-    }
-
-}
-
-calcPrecio(); */
 
 class Producto {
-  constructor(id, name, description, src, category, price) {
-    this.id = id;
-    this.name = name;
-    this.category = category;
-    this.description = description;
-    this.src = src;
-    this.price = price;
-  }
+    constructor(id, name, description, src, category, price, quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.src = src;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
 
-let prod1 = new Producto("1", "Nike Air Max", "Nike Air Max blancas", "assets/img/Air-Max1.png", "male", 9000);
-let prod2 = new Producto("2", "Nike Air Jordan", "Nike Air Jordan rojas", "assets/img/Air-Jordan2.png", "male", 9000);
-let prod3 = new Producto("3", "Nike Air Jordan", "Nike Air Jordan cromadas", "assets/img/Air-Jordan3.png", "male", 6000);
-let prod4 = new Producto("4", "Nike Air Max", "Nike Air Max Negras", "assets/img/Air-Max2.png", "female", 5000);
-let prod5 = new Producto("5", "Nike Air Max", "Nike Air Max gris", "assets/img/Air-Max3.png", "male", 8000);
-let prod6 = new Producto("6", "Nike Air Zoom", "Nike Air Zoom Blancas", "assets/img/Air-Zoom1.png", "female", 6000);
-let prod7 = new Producto("7", "Nike Venture", "Nike Air Venture azules", "assets/img/Venture1.png", "male", 8000);
-let prod8 = new Producto("8", "Nike Next", "Nike Next Nature", "assets/img/Next-Nature1.png", "female", 8000);
 
 let productList = [];
-productList.push(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8);
+
+const shoppingCart = JSON.parse(localStorage.getItem('products')) || [];
 
 const productMaker = () => {
-  productList.forEach((e) => {
-    let div = document.createElement("div");
-    div.className = "product";
-    div.setAttribute("id", e.id);
-    div.innerHTML = `<p class="epigrafe" id="name">${e.name}</p>
-                        <img alt="${e.description}" src="${e.src}" class="img-product"><button class="addCart">Agregar al carrito</button>
+    fetch('products.json')
+        .then(products => products.json())
+        .then(result => {
+
+            productList = result;
+            
+            productList.forEach(e => {
+                let div = document.createElement("div");
+                div.className = "product";
+                div.setAttribute("id", e.id);
+                div.innerHTML = `<p class="epigrafe" id="name">${e.name}</p>
+                        <img alt="${e.description}" src="${e.src}" class="img-product"><button id="button${e.id}" class="addCart">Agregar al carrito</button>
                         <p class="epigrafe" id="price">$${e.price}</p>
                         `;
-    document.querySelector(".products").appendChild(div);
-  });
-};
+                document.querySelector(".products").appendChild(div);
+
+
+                const button = document.getElementById(`button${e.id}`)
+                button.addEventListener("click", () => {
+                    agregandoAlCarrito(e.id)
+                })
+
+            });
+        })
+}
 
 productMaker();
+
+const agregandoAlCarrito = (productId) => {
+    let buscarProducto = productList.find(elemento => elemento.id === productId)
+    if (buscarProducto) {
+        let productoAgregado = shoppingCart.find(elemento => elemento.id == buscarProducto.id)
+        if (productoAgregado) {
+            productoAgregado.quantity += 1
+        } else {
+            shoppingCart.push(new Producto(buscarProducto.id, buscarProducto.name, buscarProducto.description, buscarProducto.src, buscarProducto.category, buscarProducto.price, buscarProducto.quantity))
+        }
+    }
+    localStorage.setItem('products', JSON.stringify(shoppingCart))
+    cartQuantity()
+
+};
+
+
+let quantitySpan = document.querySelector(".cart_quantity");
+
+
+function cartQuantity() {
+
+    let total = shoppingCart.reduce((acc, e) => acc + e.quantity, 0)
+    quantitySpan.innerHTML = `${total}`;
+    localStorage.setItem('products', JSON.stringify(shoppingCart))
+}
+
+
+cartQuantity()
+
+let formulario = document.querySelector(".contact")
+let sendBtn = document.querySelector("#submit-btn")
+
+formulario.addEventListener("submit", e => {
+    e.preventDefault();
+    Swal.fire({ icon: 'success', background: 'rgb(11, 4, 15)', confirmButtonColor: 'rgb(60, 38, 70)', color: '#eeeeed', title: 'Enviado' })
+}
+);
